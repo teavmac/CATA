2004b1090dbb51e07cde16ec13bcacf8e8e40f37
teavamc <teavamc@163.com>
2019-02-26 21:25:11 +0800

    create springboot blank project

---
+++ cata-admin/src/main/java/com/zc/cataadmin/CataAdminApplication.java
@@ -0,0 +1,13 @@
+package com.zc.cataadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CataAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CataAdminApplication.class, args);
+    }
+
+}
